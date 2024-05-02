3f0c23d45cd36a4011d0481dc08426a5d972e036
aaron <amccarthy9904@gmail.com>
2024-05-01 17:13:30 -0700

    some final touches

--- scrape.py
+++ scrape.py
@@ -5,6 +5,9 @@ import string
 from collections import Counter
 
 
+"""
+    Initiates scraping of Microsoft's Wikipedia Page
+"""
 def start():
 
     number = input("""
@@ -12,7 +15,8 @@ def start():
     /   _ _    | _ _  /   _  _|. _  _   /  |_  _ || _ _  _  _ 
     \__| (_|\)/|(-|   \__(_)(_||| )(_)  \__| )(_|||(-| )(_)(- 
                                    _/                   _/    
-    - Aaron McCarthy                                                            
+    - Aaron McCarthy
+    - https://github.com/amccarthy9904/microsoft_web_scraper                                                            
                                                             
     Enter number of words to return (default = 10)\n""")
     number = int(number) if number else 10
@@ -30,13 +34,11 @@ def start():
         text += curr.text_content() + ' '
         curr = curr.getnext()
 
-    print(text)
     text = text.replace('.', ' ')
     text = text.replace(',', ' ')
     text = text.translate(str.maketrans(' ', ' ', string.punctuation))
     text = text.translate(str.maketrans(' ', ' ', string.digits))
     text = text.split(' ')
-    # print(text)
     c = Counter()
     l = 0
     for word in text:
@@ -45,12 +47,7 @@ def start():
             l = max(l, len(clean_word))
             c[clean_word] += 1
     
-    # table = [[1, 2222, 30, 500], [4, 55, 6777, 1]]
-    # for row in table:
-    #     print('| {:1} | {:^4} | {:>4} | {:<3} |'.format(*row))
     for pair in c.most_common(number):
         print('| {:14} | {:3} |'.format(*pair))
-
-
 
 start()
